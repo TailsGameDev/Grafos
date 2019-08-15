620218aad454948f2fa733f680de28fcce2be415
Vinicius Guedes dos Santos <vinicius.g.santos@ufsc.br>
2019-08-15 11:45:06 -0300

    Inicia leitura de arquivo

---
+++ grafo.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+class Grafo:
+
+	def ler(self, arquivo):
+	    file = open(arquivo)
+	    inutil, vertices = file.readline.split()
+	    for i in range(vertices):
+	        line = file.readline()
+#ler()
+f = open("")
