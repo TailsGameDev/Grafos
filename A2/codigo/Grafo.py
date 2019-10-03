600979fa61e996c4be0b038e76ba9ee113483bdc
Vinicius Guedes dos Santos <vinicius_gs_lp@hotmail.com>
2019-10-03 11:33:23 -0300

    Criada pasta para a A1

--- A2/codigo/Grafo.py
+++ A2/codigo/Grafo.py
@@ -15,7 +15,7 @@ class Grafo:
             indice = int(linha[0])
             linha.pop(0)
             self.rotulos[indice - 1] = " ".join(linha)
-        arquivo.readline()
+        arco_ou_aresta = arquivo.readline()
         while True:
             linha = arquivo.readline()
             if linha == "":
@@ -25,13 +25,14 @@ class Grafo:
             vertice2 = int(vertice2)
             peso = float(peso)
             self.matriz[vertice1 - 1][vertice2 - 1] = peso
-            self.matriz[vertice2 - 1][vertice1 - 1] = peso
+            if arco_ou_aresta == "*edges":
+                self.matriz[vertice2 - 1][vertice1 - 1] = peso
             self.arestas += 1
         arquivo.close()
 
     def qtdVertices(self):
         return len(self.rotulos)
-    
+
     def qtdArestas(self):
         return self.arestas
 
@@ -41,11 +42,21 @@ class Grafo:
             if self.matriz[vertice - 1][i] != 0:
                 grau += 1
         return grau
-    
+
     def rotulo(self, vertice):
         return self.rotulos[vertice - 1]
 
     def vizinhos(self, vertice):
+        return vizinhos_saintes(vertice)
+
+    def vizinhos_saintes(self, vertice):
+        lista_vizinhos = []
+        for i in range(self.matriz.shape[0]):
+            if self.matriz[i][vertice - 1] != 0:
+                lista_vizinhos.append(i + 1)
+        return lista_vizinhos
+
+    def vizinhos_entrantes(self, vertice):
         lista_vizinhos = []
         for i in range(self.matriz.shape[0]):
             if self.matriz[vertice - 1][i] != 0:
