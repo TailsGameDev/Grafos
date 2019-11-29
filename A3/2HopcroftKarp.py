3ce258fe4b07322b9211393531391c06f53f1466
Caina <cainacc@hotmail.com>
2019-11-28 23:50:10 -0300

    falei q dava ateh meia noite

--- A3/2HopcroftKarp.py
+++ A3/2HopcroftKarp.py
@@ -27,21 +27,39 @@ def BFS(G, mate, D):
             Q.append(x)
         else:
             D[x] = inf
-    D[0] = inf
+    setD(D, None, inf)
     while len(Q) > 0:
         x = Q.pop(0)
-        if D[x] < D[0]:
+        if getD(D, x) < getD(D, None):
             for y in G.vizinhos(x):
-                if D[mate[y]] == inf:
-                    D[mate[y]] = D[x] + 1
+                if getD(D, mate[y]) == inf:
+                    setD(D, mate[y], getD(D, x) + 1)  # D[mate[y]] = D[x] + 1
                     Q.append(mate[y])
     return D[0]
+
+
+def getD(D, key):
+    if key == None:
+        return D[0]
+    else:
+        return D[key]
+
+
+def setD(D, key, value):
+    if key == None:
+        D[0] = value
+        if D[0] > inf:
+            D[0] = inf  # nao ouso aumentar mais que o infinito
+    else:
+        D[key] = value
+        if D[key] > inf:
+            D[key] = inf  # nao ouso aumentar mais que o infinito
 
 
 def DFS(G, mate, x, D):
     if x != None:
         for y in G.vizinhos(x):
-            if D[mate[y]] == D[x] + 1:
+            if getD(D, mate[y]) == getD(D, x) + 1:
                 if DFS(G, mate, mate[y], D) == True:
                     mate[y] = x
                     mate[x] = y
@@ -53,4 +71,8 @@ def DFS(G, mate, x, D):
 
 G = Grafo("emparelhamento_maximo/gr128_10.gr")
 [m, mate] = HopcroftKarp(G)
-print(G.vertices)
+print("valor do emparelhamento maximo: " + str(m))
+print("\narestas pertencentes ao emparelhamento:")
+for i in range(len(mate)):
+    if mate[i] != None:
+        print(str(i) + "-" + str(mate[i]))
